d85beb0778b0985c7bb547a58a40e8c12a5e7f75
rhday <54642787+rhday@users.noreply.github.com>
2021-03-04 12:05:43 +0000

    Done.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,33 @@
 import React from 'react';
 
 class Form extends React.Component {
+  render() {
+    return (
+      <div>
+        <form>
+          <input
+            type="text"
+            name="firstName"
+            onChange={event => this.props.handleChange(event)}
+            value={this.props.formData.firstName}
+          />
+          <input
+            type="text"
+            name="lastName"
+            onChange={event => this.props.handleChange(event)}
+            value={this.props.formData.lastName}
+          />
+        </form>
+      </div>
+    )
+  }
+}
+
+/*class Form extends React.Component {
   state = {
     firstName: "John",
-    lastName: "Henry"
+    lastName: "Henry",
+    submittedData: []
   }
 
   handleFirstNameChange = event => {
@@ -18,14 +42,36 @@ class Form extends React.Component {
     })
   }
 
+  handleSubmit = event => {
+    event.preventDefault()
+    let formData = { firstName: this.state.firstName, lastName: this.state.lastName }
+    let dataArray = this.state.submittedData.concat(formData)
+    this.setState({submittedData: dataArray})
+  }
+
+  listOfSubmissions = () => {
+    return this.state.submittedDate.map(data => {
+      return <div><span>{data.FirstName}</span> <span>{data.lastName}</span></div>
+    })
+  }
+
   render() {
     return (
-      <form>
-        <input type="text" name="firstName" onChange={event => this.handleFirstNameChange(event)} value={this.state.firstName} />
-        <input type="text" name="lastName" onChange={event => this.handleLastNameChange(event)} value={this.state.lastName} />
+      <form onSubmit={event => this.handleSubmit(event)}>
+        <input
+          type="text"
+          onChange={event => this.handleFirstNameChange(event)}
+          value={this.state.firstName}
+        />
+        <input
+          type="text"
+          onChange={event => this.handleLastNameChange(event)}
+          value={this.state.lastName}
+        />
+        <input type="submit" />
       </form>
     )
-  }
-}
+  } 
+}*/
 
 export default Form;
---
+++ src/components/ParentComponent.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import DisplayData from './DisplayData';
+import Form from './Form'
+
+class ParentComponent extends React.Component {
+  state = {
+    firstName: "",
+    lastName: "",
+  }
+
+  /*handleFirstNameChange = event => {
+    this.setState({
+      firstName: event.target.value
+    })
+  }
+
+  handleLastNameChange = event => {
+    this.setState({
+      lastName: event.target.value
+    })
+  }*/
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+
+  render() {
+    return (
+    <div>
+      <Form
+        formData={this.state}
+        handleChange={this.handleChange}
+      />
+      <DisplayData formData={this.state} />
+    </div>
+    )
+  }
+}
+
+export default ParentComponent; 
